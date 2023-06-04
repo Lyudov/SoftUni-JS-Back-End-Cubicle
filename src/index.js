64f196c08afe99b5cf613f68fa35408f7f9abb13
Lyudov <dlyudov@gmail.com>
2023-06-04 23:39:20 +0300

    Connect DB

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
+const dbConnect = require('./config/dbConfig');
 const routes = require('./routes');
 
 const app = express();
 
 const PORT = 5000;
 
-expressConfig(app);
 //require('./config/expressConfig')(app);
+expressConfig(app);
 
 handlebarsConfig(app);
 //app.get('/', homeController.getHome);
+
+dbConnect()
+    .then(() => console.log('DB Connected successfully'))
+    .catch(err => console.log('DB error: ', err));
 
 app.use(routes);
 
