84deda0b3abe4013ce8069399b361d192255d840
Lyudov <dlyudov@gmail.com>
2023-06-10 18:24:41 +0300

    Add relation from cube to accessory

--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -2,4 +2,5 @@ const Accessory = require('../models/Accessory');
 
 exports.getAll = () => Accessory.find();
 
+// Second create type
 exports.create = (accessoryData) => Accessory.create(accessoryData);
--- src/managers/cubeManagers.js
+++ src/managers/cubeManagers.js
@@ -3,26 +3,37 @@ const Cube = require('../models/Cube');
 exports.getAll = async (search, from, to) => {
     let result = await Cube.find().lean();
 
-if(search) {
-result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
-}
+    if (search) {
+        result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
+    }
 
-if(from) {
-result = result.filter(cube => cube.difficultyLevel >= Number(from));
-}
+    if (from) {
+        result = result.filter(cube => cube.difficultyLevel >= Number(from));
+    }
 
-if(to) {
-result = result.filter(cube => cube.difficultyLevel <= Number(to));
-}
+    if (to) {
+        result = result.filter(cube => cube.difficultyLevel <= Number(to));
+    }
 
-return result;
+    return result;
 }
 exports.getOne = (cubeId) => Cube.findById(cubeId);
 
+// First create type
 exports.create = async (cubeData) => {
-   const cube = new Cube(cubeData);
+    const cube = new Cube(cubeData);
 
-   await cube.save();
-    
+    await cube.save();
+
     return cube;
 };
+
+exports.attachAccessory = async (cubeId, accessoryId) => {
+
+    //    return Cube.findByIdAndUpdate(cubeId, {$push: {accessories: accessoryId}});
+
+    const cube = await Cube.findById(cubeId);
+    cube.accessories.push(accessoryId);
+    return cube.save();
+
+}
