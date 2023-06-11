273e0801d7cd84b1875f0550b5ef70502e238571
Lyudov <dlyudov@gmail.com>
2023-06-11 19:53:03 +0300

    Create user login logic

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,12 +10,20 @@ router.get('/register', (req, res) => {
 router.post('/register', async (req, res) => {
     const { username, password, repeatPassword } = req.body;
 
-   await userManager.register({username,password,repeatPassword});
+    await userManager.register({ username, password, repeatPassword });
     res.redirect('/users/login');
 });
 
-router.get('/login', (req,res)=>{
+router.get('/login', (req, res) => {
     res.render('users/login');
-})
+});
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await userManager.login(username, password);
+    console.log(user);
+    res.redirect('/');
+});
 
 module.exports = router;
